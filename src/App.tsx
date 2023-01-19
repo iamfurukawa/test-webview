13d3121711c0099088e8e22233a27a5fd38acc90
viniFurukawa <vinicius.carvalho@opus-software.com.br>
2023-01-19 19:15:01 -0300

    add event

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,16 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const sendEvent = () => {
+    try {
+      messageHandler.postMessage(count);
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setCount((count) => count + 1)
+    }
+  }
+
   return (
     <div className="App">
       <div>
@@ -17,7 +27,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => sendEvent()}>
           count is {count}
         </button>
         <p>
