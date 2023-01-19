fa06e7b550cf885db9a659d380e65201cc38855b
viniFurukawa <vinicius.carvalho@opus-software.com.br>
2023-01-19 19:35:41 -0300

    add event

--- src/App.tsx
+++ src/App.tsx
@@ -6,39 +6,38 @@ function App() {
   const [count, setCount] = useState(0)
 
   const sendEvent = () => {
-    try {
-      window.messageHandler.postMessage('Hello from JS');
-    } catch (error) {
-      console.error(error)
-    } finally {
-      setCount((count) => count + 1)
-    }
+    setCount((count) => count + 1)
+    if (window.messageHandler && window.messageHandler.postMessage)
+      window.messageHandler.postMessage('Hello from JS' + count);
+    else
+      console.log('not executed.')
   }
+}
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => sendEvent()}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+return (
+  <div className="App">
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://reactjs.org" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+    <h1>Vite + React</h1>
+    <div className="card">
+      <button onClick={() => sendEvent()}>
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
       </p>
     </div>
-  )
+    <p className="read-the-docs">
+      Click on the Vite and React logos to learn more
+    </p>
+  </div>
+)
 }
 
 export default App
